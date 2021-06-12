5fb6c197c3c95218a74c0db94869a1e65987c0c8
Banana Viking <bananavikingcoder@gmail.com>
2021-06-12 12:32:25 -0500

    Add TimelineView to MetricsView

--- WatchWorkouts WatchKit Extension/Views/MetricsView.swift
+++ WatchWorkouts WatchKit Extension/Views/MetricsView.swift
@@ -11,44 +11,66 @@ struct MetricsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ElapsedTimeView(
-                elapsedTime: workoutManager.builder?.elapsedTime ?? 0,
-                showSubseconds: true
-            ).foregroundColor(.yellow)
-            Text(
-                Measurement(
-                    value: workoutManager.activeEnergy,
-                    unit: UnitEnergy.kilocalories
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .workout,
-                        numberFormat: .numeric(precision: .fractionLength(0))
+        TimelineView(
+            MetricsTimelineSchedule(from: workoutManager.builder?.startDate ?? Date())
+        ) { context in
+            VStack(alignment: .leading) {
+                ElapsedTimeView(
+                    elapsedTime: workoutManager.builder?.elapsedTime ?? 0,
+                    showSubseconds: true
+                ).foregroundColor(.yellow)
+                Text(
+                    Measurement(
+                        value: workoutManager.activeEnergy,
+                        unit: UnitEnergy.kilocalories
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .workout,
+                            numberFormat: .numeric(precision: .fractionLength(0))
+                        )
                     )
                 )
+                Text(
+                    workoutManager.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm"
+                )
+                Text(
+                    Measurement(
+                        value: workoutManager.distance,
+                        unit: UnitLength.meters
+                    ).formatted(.measurement(width: .abbreviated, usage: .road))
+                )
+            }
+            .font(.system(.title, design: .rounded)
+                    .monospacedDigit()
+                    .lowercaseSmallCaps()
             )
-            Text(
-                workoutManager.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm"
-            )
-            Text(
-                Measurement(
-                    value: workoutManager.distance,
-                    unit: UnitLength.meters
-                ).formatted(.measurement(width: .abbreviated, usage: road))
-            )
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
         }
-        .font(.system(.title, design: .rounded)
-                .monospacedDigit()
-                .lowercaseSmallCaps()
-        )
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .padding()
     }
 }
 
 struct MetricsView_Previews: PreviewProvider {
     static var previews: some View {
         MetricsView()
+    }
+}
+
+private struct MetricsTimelineSchedule: TimelineSchedule {
+    var startDate: Date
+    
+    init(from startDate: Date) {
+        self.startDate = startDate
+    }
+    
+    func entries(from startDate: Date, mode: TimelineScheduleMode) -> PeriodicTimelineSchedule.Entries {
+        PeriodicTimelineSchedule(
+            from: self.startDate,
+            by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0)
+        ).entries(
+            from: startDate,
+            mode: mode
+        )
     }
 }
